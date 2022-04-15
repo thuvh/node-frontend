2c3216c4be3e8f640fd35bd2fdb14c44c5a6c5fa
Hoai-Thu Vuong <thuvh87@gmail.com>
2022-04-15 14:23:41 +0700

    fix for node 16 and build with arg

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -3,17 +3,15 @@
 set -e
 set -x
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-NODE_VERSION=10 docker build -t thuvh/node-frontend:10 .
+NODE_VERSION=10; docker build --build-arg "NODE_VERSION=$NODE_VERSION" -t thuvh/node-frontend:$NODE_VERSION .
 
-NODE_VERSION=16.13.0 docker build -t thuvh/node-frontend:${NODE_VERSION} .
-NODE_VERSION=16 docker build -t thuvh/node-frontend:${NODE_VERSION} .
-NODE_VERSION=16 docker tag thuvh/node-frontend:${NODE_VERSION} thuvh/node-frontend:latest
+NODE_VERSION=16.13.0; docker build --build-arg "NODE_VERSION=$NODE_VERSION" -t thuvh/node-frontend:$NODE_VERSION .
+NODE_VERSION=16; docker build --build-arg "NODE_VERSION=$NODE_VERSION" -t thuvh/node-frontend:$NODE_VERSION .
+NODE_VERSION=16; docker tag thuvh/node-frontend:$NODE_VERSION thuvh/node-frontend:latest
 
 docker push thuvh/node-frontend:10
 docker push thuvh/node-frontend:16.13.0
 docker push thuvh/node-frontedn:16
 docker push thuvh/node-frontend:latest
-
-
